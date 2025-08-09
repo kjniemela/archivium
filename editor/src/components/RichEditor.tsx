25653a0436ee98bc750cc3286d8dec0d79e1b682
kjniemela <johannes@niemela.se>
2025-08-09 13:15:25 -0500

    add Aside extension

--- editor/src/components/RichEditor.tsx
+++ editor/src/components/RichEditor.tsx
@@ -1,9 +1,13 @@
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import { Aside } from '../extensions/Aside';
 
 export default function RichEditor({ content }: { content?: string }) {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      Aside,
+    ],
     content,
     onUpdate: ({ editor }) => {
       console.log(editor.getJSON());
