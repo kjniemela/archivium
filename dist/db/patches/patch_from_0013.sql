0a07a8ee04243387268803e2b306e46319d6ddc3
kjniemela <johannes@niemela.se>
2025-08-18 19:48:42 -0500

    fix patch 13

--- dist/db/patches/patch_from_0013.sql
+++ dist/db/patches/patch_from_0013.sql
@@ -3,4 +3,6 @@ UPDATE schema_version SET version = 14, comment = 'Allow user custom theme, add 
 ALTER TABLE user
 ADD COLUMN custom_theme JSON DEFAULT NULL;
 
+SET SQL_SAFE_UPDATES = 0; -- Not optimal, but anything else I can think of would be ugly
 UPDATE userplan SET plan = 4 WHERE plan = 3;
+SET SQL_SAFE_UPDATES = 1;
