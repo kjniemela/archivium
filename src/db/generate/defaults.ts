c207bb517e497e0151252c8086e1b3327552433d
Johannes Niemelä <johannes@niemela.se>
2025-08-25 12:44:11 -0500

    fix generate DB

--- src/db/generate/defaults.ts
+++ src/db/generate/defaults.ts
@@ -1,4 +1,17 @@
-import { Item, ItemEvent } from "../../api/models/item";
+import { Item, ItemEvent } from '../../api/models/item';
+import { IndexedDocument } from '../../lib/tiptapHelpers';
+
+export function unformattedTiptapDocument(text: string): IndexedDocument {
+  return {
+    text,
+    structure: [{
+      type: 'paragraph',
+      attrs: {},
+      marks: [],
+      content: [{ start: 0, end: text.length, type: 'text', attrs: {}, marks: [] }],
+    }],
+  };
+}
 
 export const defaultUniverseData = {
   cats: {
@@ -16,7 +29,7 @@ export const defaultUniverseData = {
 
 export const defaultItemData = {
   article: {
-    body: '# Markdown Test\n\n- **Bold**\n- *Italics*\n- _Italics 2_\n- __Underline__\n- ~~Strikethrough~~\n- [Link](@test-character)\n- Lists',
+    body: unformattedTiptapDocument('# Markdown Test\n\n- **Bold**\n- *Italics*\n- _Italics 2_\n- __Underline__\n- ~~Strikethrough~~\n- [Link](@test-character)\n- Lists'),
   },
   character(age=0, parent: Item | null = null, child: Item | null = null) {
     return {
@@ -28,13 +41,13 @@ export const defaultItemData = {
         parents: parent ? [{ parent_shortname: parent.shortname, parent_title: parent.title }] : [],
         children: child ? [{ child_shortname: child.shortname, child_title: child.title }] : [],
       }),
-      body: 'This is a test character.',
+      body: unformattedTiptapDocument('This is a test character.'),
       lineage: { title: 'Lineage' },
       timeline: { title: 'Timeline' },
     };
   },
   event: {
-    body: 'This is a test event.',
+    body: unformattedTiptapDocument('This is a test event.'),
     _tables: (self: Item) => ({
       events: [
         { event_title: null, abstime: Math.round(Math.random() * 31557600000), src_id: self.id, src_shortname: self.shortname, src_title: self.title },
