c88bc637c3097170a5ed757902deae4f7c58c348
Johannes Niemelä <johannes@niemela.se>
2026-01-18 09:22:11 -0600

    include co-parents

--- dist/lib/familyTree.js
+++ dist/lib/familyTree.js
@@ -4,7 +4,7 @@ exports.layoutFamilyTree = layoutFamilyTree;
 function _layoutDown(start, family) {
     const tree = {
         width: 0,
-        members: { [start]: { row: 0, col: 0, cStart: 0, cEnd: 0 } },
+        members: { [start]: { row: 0, col: 0 } },
     };
     const childTrees = [];
     for (const { child_shortname } of family[start].children) {
@@ -12,22 +12,47 @@ function _layoutDown(start, family) {
         tree.width += childTree.width;
         childTrees.push(childTree);
     }
-    let offset = 0;
+    const spouses = {};
+    for (const { child_shortname } of family[start].children) {
+        for (const { parent_shortname } of family[child_shortname].parents) {
+            if (!(parent_shortname in spouses) && parent_shortname !== start) {
+                spouses[parent_shortname] = true;
+            }
+        }
+    }
+    const startGenWidth = 2 + Object.keys(spouses).length * 2;
+    console.log(spouses);
+    let offset = Math.max(0, startGenWidth - tree.width) / 2;
     for (const childTree of childTrees) {
         for (const member in childTree.members) {
             childTree.members[member].col += offset;
-            childTree.members[member].cStart += offset;
-            childTree.members[member].cEnd += offset;
+            if (childTree.members[member].cStart !== undefined)
+                childTree.members[member].cStart += offset;
+            if (childTree.members[member].cEnd !== undefined)
+                childTree.members[member].cEnd += offset;
             childTree.members[member].row += 1;
         }
         offset += childTree.width;
         tree.members = { ...tree.members, ...childTree.members };
     }
-    if (tree.width === 0)
-        tree.width = 2;
-    tree.members[start].col += tree.width / 2;
-    tree.members[start].cStart = Math.min(tree.members[start].col, ...family[start].children.map(({ child_shortname }) => tree.members[child_shortname].col));
-    tree.members[start].cEnd = Math.max(tree.members[start].col, ...family[start].children.map(({ child_shortname }) => tree.members[child_shortname].col));
+    tree.width = Math.max(tree.width, startGenWidth);
+    tree.members[start].col += (tree.width / 2 - startGenWidth / 2) + 1;
+    let i = 0;
+    for (const spouse in spouses) {
+        i++;
+        tree.members[spouse] = {
+            row: 0,
+            col: tree.members[start].col + i * 2,
+        };
+    }
+    if (childTrees.length > 0) {
+        const [cStart, cEnd] = [
+            Math.min(tree.members[start].col, ...family[start].children.map(({ child_shortname }) => tree.members[child_shortname].col)),
+            Math.max(tree.members[start].col, ...family[start].children.map(({ child_shortname }) => tree.members[child_shortname].col), ...Object.keys(spouses).map(spouse => tree.members[spouse].col)),
+        ];
+        tree.members[start].cStart = cStart;
+        tree.members[start].cEnd = cEnd;
+    }
     console.log(tree);
     return tree;
 }
