70e85d3c1e9b20c496ff75bdf9a421fa536839d3
Johannes Niemelä <johannes@niemela.se>
2026-01-21 02:06:21 -0600

    record cypress runs

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "ury9q9",
   e2e: {
     baseUrl: 'http://localhost:33004',
     setupNodeEvents(on, config) {
